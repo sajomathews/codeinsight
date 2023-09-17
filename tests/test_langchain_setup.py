f82a9bc6b08567826073c94bf7e0c78d9267f0a5
Sajo Mathews <sajomathews@gmail.com>
2023-09-17 20:21:49 +0000

    feat: add langchain and llama-cpp as dependencies
    add test cases to check they are working properly
    
    fixes: #2

---
+++ tests/test_langchain_setup.py
@@ -0,0 +1,10 @@
+# tests/test_langchain_setup.py
+
+import langchain
+
+
+def test_langchain_setup():
+    # Check if the Langchain module can be imported
+    assert langchain is not None
+
+    # Add more assertions to test Langchain functionality, if needed
---
+++ tests/test_llamacpp_setup.py
@@ -0,0 +1,27 @@
+# tests/test_llama_cpp.py
+import pytest
+from langchain import LlamaCpp  # Import the LlamaCpp module
+
+
+@pytest.fixture
+def model_path() -> str:
+    return "models/ggml-model-codellama-13B-q4_0.gguf"
+
+
+@pytest.fixture
+def llama_cpp_model(model_path: str):
+    # Initialize the LlamaCpp model (adjust the path as needed)
+    return LlamaCpp(model_path=model_path)
+
+
+def test_llama_cpp_with_langchain(llama_cpp_model: LlamaCpp):
+    # Example: Generate a code summary using LlamaCpp and Langchain
+    input_code = """
+    def greet(name):
+    """
+
+    # Use LlamaCpp to generate a code summary
+    code_summary = llama_cpp_model.predict(input_code)
+
+    # Assert that the code summary is not empty or None
+    assert code_summary is not None and code_summary.strip() != ""
