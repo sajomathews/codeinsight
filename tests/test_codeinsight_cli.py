1011de0779b4cd6eacd5523a0de9f58b294b9ac8
Sajo Mathews <sajomathews@gmail.com>
2023-09-18 10:25:30 +0000

    test(tests): add more test cases for the CLI
    fixes: #6

--- tests/test_codeinsight_cli.py
+++ tests/test_codeinsight_cli.py
@@ -1,13 +1,36 @@
-import typer
+import pytest
 from typer.testing import CliRunner
 from codeinsight.cli import app  # Import the command function from your script
+from tempfile import TemporaryDirectory
+from pathlib import Path
 
 
-def test_summarize_command():
-    runner = CliRunner()
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
 
+
+def test_summarize_no_args_fail(runner: CliRunner):
     # Use the `runner.invoke` method to run the command with arguments
     result = runner.invoke(app)
 
-    # Check if the command succeeded and the output is as expected
+    # Check if the command failed as expected
     assert result.exit_code == 2
+
+
+def test_summarize_with_non_existent_input_dir(runner: CliRunner):
+    result = runner.invoke(app, ["no_such_dir"])
+
+    # Check if the command failed as expected
+    assert result.exit_code == 2
+
+
+def test_summarize_with_valid_dir(runner: CliRunner):
+    with TemporaryDirectory() as tmpdir:
+        result = runner.invoke(app, [".", f"--output-dir={tmpdir}"])
+
+        # Check dir contains some files
+        assert list(Path(tmpdir).glob("*.txt")) is not None
+
+    # Check if the command was successful
+    assert result.exit_code == 0
